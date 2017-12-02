631636b138a30f934ed520277c723b912bc90a0a
mfathirirhas <fathir.irhas@outlook.com>
2017-12-02 14:53:25 +0700

    Create js folder in assets to contain all javascript files. Remove inline js code in index.html. Add javascript file 'mfi.js' to contain all standard js libraries for this blog. Include 'mfi.js' inside head.html.

---
+++ assets/js/mfi.js
@@ -0,0 +1,34 @@
+/**
+ * mfi.js contains all standard javascript libraries for this blog development.
+ * Author: Muhammad Fathir Irhas
+ * License: MIT
+ * ------------------------------------------------------------------------------
+ */
+
+
+
+/**
+ * @function dateTime 
+ * @description Print current date and time
+ * @author Muhammad Fathir Irhas
+ * 
+ */
+function dateTime() {
+    var today = new Date();
+    var y = today.getFullYear();
+    var m = today.getMonth() + 1;
+    var d = today.getDate();
+    var h = today.getHours();
+    var mnt = today.getMinutes();
+    var s = today.getSeconds();
+    mnt = checkTime(mnt);
+    s = checkTime(s);
+
+    document.getElementById('txt').innerHTML = y + " / " + m + " / " + d + " - " + h + ":" + mnt + ":" + s;
+    var t = setTimeout(dateTime,500);
+}
+
+function checkTime(i){
+    if (i < 10) {i = "0" + i;} 
+    return i;
+}
